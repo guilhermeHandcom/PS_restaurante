cce166ed1f405fd7f7b9eb1413f176f5a24fe36e
Pedro Felipe <pedro@handcom.com.br>
2014-12-01 20:30:20 -0200

    Finalização do calculo de faturamento

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,17 @@
 module ApplicationHelper
 	def retornaValorComanda(comanda)
 
-		@comanda_produtos = ComandaProduto.where("comanda_id = ?", comanda.id)
-		if !@comanda_produtos.blank?
-			p "============================ #{@comanda_produtos.length} "
+		@comanda_produto = ComandaProduto.where("comanda_id = ?", comanda.id)
+
+		if !comanda.produtos.blank?
+			@valorTotal = 0	
+			comanda.comanda_produtos.each do |cp|
+				@valorTotal += (cp.produto.valor * cp.quantidade)
+			end
+
+			return @valorTotal
 		else
-		end		
-
-		# if !comanda.produtos.blank?
-		# 	@valorTotal = 0	
-		# 	comanda.produtos.each do |p|
-		# 		puts '##################'+p.id.to_s
-		# 		#cp.produto.each do |p|
-		# 		#	@valorTotal = @valorTotal + (p.valor * c_p.quantidade)
-		# 		#end
-		# 	end
-
-		# 	return @valorTotal
-		# else
-		# 	return 'Nenhum produto encontrado.'
-		# end
+			return 'Nenhum produto encontrado.'
+		end
 	end
 end
